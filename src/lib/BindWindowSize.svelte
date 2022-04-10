05124a3cb5d650649b5bd751a0b5467c3c9375ca
niwla23 <46248939+niwla23@users.noreply.github.com>
2022-04-10 11:29:45 +0000

    Deploying to gh-pages from @ Lobby-Hoes/wo-ist-vulva@77acea462304fff0bf6f6c962ee4da6671fa5893 🚀

--- src/lib/BindWindowSize.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-    import { windowHeight, windowWidth } from './stores';
-</script>
-
-<svelte:window bind:innerWidth={$windowWidth} bind:innerHeight={$windowHeight} />
--- src/lib/timeout.ts
+++
@@ -1,3 +0,0 @@
-export async function timeout(ms: number) {
-    return new Promise<void>((r) => setTimeout(r, ms));
-}
