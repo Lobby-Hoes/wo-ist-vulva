47365eb2410958511a04f8f1a56a39fe30d53245
lobbyhoe12345 <lobbyhoe12345@gmail.com>
2022-04-10 14:48:29 +0200

    feat: decrease audio volume

--- src/lib/Level.svelte
+++ src/lib/Level.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
     import RandomVulva from './RandomVulva.svelte';
-    import { fade } from 'svelte/transition';
+    import {fade} from 'svelte/transition';
     import ScoreDisplay from './ScoreDisplay.svelte';
-    import { calcPlaygroundSize } from './stores';
-    import { onMount } from 'svelte';
+    import {calcPlaygroundSize} from './stores';
+    import {onMount} from 'svelte';
 
     export let vulvaPositions: [x: number, y: number][] = [];
     export let backgroundImage: string = '';
@@ -65,30 +65,32 @@
                 backgroundAudioRef.play();
             });
         }
+
+        backgroundAudioRef.volume = vulvaFoundAudioRef.volume = levelCompletedAudioRef.volume = 0.1;
     });
 </script>
 
 <div style={levelStyle}>
-    <ScoreDisplay found={vulvaPositionIndex} of={vulvaPositions.length} />
+    <ScoreDisplay found={vulvaPositionIndex} of={vulvaPositions.length}/>
 
     {#key [backgroundImage]}
-        <img in:fade bind:this={imgRef} src={backgroundImage} />
+        <img in:fade bind:this={imgRef} src={backgroundImage}/>
     {/key}
 
     {#if imgRef}
         {#key vulvaPositionIndex}
             <div in:fade style="position: absolute; left: {x}%; top: {y}%; ">
-                <RandomVulva on:click={handleClick} {randomNumber} />
+                <RandomVulva on:click={handleClick} {randomNumber}/>
             </div>
         {/key}
     {/if}
 </div>
 
-<audio bind:this={backgroundAudioRef} src={music} autoplay loop />
-<audio bind:this={vulvaFoundAudioRef} src={vulvaFoundSound} />
-<audio bind:this={levelCompletedAudioRef} src={levelCompletedSound} />
+<audio bind:this={backgroundAudioRef} src={music} autoplay loop/>
+<audio bind:this={vulvaFoundAudioRef} src={vulvaFoundSound}/>
+<audio bind:this={levelCompletedAudioRef} src={levelCompletedSound}/>
 
-<svelte:window bind:innerWidth bind:innerHeight />
+<svelte:window bind:innerWidth bind:innerHeight/>
 
 <style>
     img {
